4f63502424202a28d1102f3d49bec99da20f8afd
jonathanelruso <jonathanmoreraapaza@gmail.com>
2026-05-07 12:18:06 +0100

    first commit

---
+++ dashboard-demo-backend/src/main/java/jonathandev/dashboard_demo_backend/DashboardDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package jonathandev.dashboard_demo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardDemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardDemoBackendApplication.class, args);
+	}
+
+}
---
+++ dashboard-demo-backend/src/test/java/jonathandev/dashboard_demo_backend/DashboardDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jonathandev.dashboard_demo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardDemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
